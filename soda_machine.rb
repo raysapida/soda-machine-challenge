711dbf83bc132f05faffef5081bd6be3601f526c
Raymond Sapida <ray.sapida@gmail.com>
2016-05-09 14:38:57 -0700

    Makes the tests pass for the three empty methods

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,19 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.size
   end
 
   def find_soda(soda_brand)
+    @sodas.find {|soda| soda.brand == soda_brand }
   end
 
   def sell(soda_brand)
+    soda = find_soda(soda_brand)
+    if soda
+      @sodas.delete(soda)
+      @cash += soda.price
+    end
   end
 
 end
